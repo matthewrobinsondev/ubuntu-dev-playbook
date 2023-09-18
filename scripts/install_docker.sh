f8f1754ca96c13680bb306b64eacee2bc6400bc9
Matt Robinson <matthewrobinsondev@gmail.com>
2023-09-18 09:27:49 +0100

    Update docker compose v2 plugin install

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -5,10 +5,27 @@ if command -v docker &> /dev/null; then
 else
     echo "Installing Docker"
 
+    # Install Docker
     sudo apt install docker
-    sudo apt install docker-compose
     sudo groupadd docker
     sudo usermod -aG docker $USER
     sudo systemctl enable docker.service
     sudo systemctl enable containerd.service
+
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl gnupg
+    sudo install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+    # Add the repository to Apt sources:
+    echo \
+    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+    # Install Docker Compose v2
+    sudo apt-get install docker-compose-plugin
 fi
